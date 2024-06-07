0cad156cb2848d9087ce6e210f088b6c5cfbac9c
Jose Calderon <josephwantri@gmail.com>
2024-06-07 15:51:44 -0500

    feat: add Products bounded context

---
+++ chefstock-platform/Products/Application/Internal/CommandServices/ProductCommandService.cs
@@ -0,0 +1,58 @@
+﻿using chefstock_platform.Products.Domain.Model.Aggregates;
+using chefstock_platform.Products.Domain.Model.Commands;
+using chefstock_platform.Products.Domain.Repositories;
+using chefstock_platform.Products.Domain.Services;
+using chefstock_platform.Shared.Domain.Repositories;
+
+namespace chefstock_platform.Products.Application.Internal.CommandServices;
+
+    public class ProductCommandService(IProductRepository productRepository, IUnitOfWork unitOfWork) : IProductCommandService
+    {
+        public async Task<Product?> Handle(CreateProductCommand command)
+        {
+            var product = new Product(command);
+            try
+            {
+                await productRepository.AddAsync(product);
+                await unitOfWork.CompleteAsync();
+                return product;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An error occurred while creating the product: {e.Message}");
+                throw;
+            }
+        }
+
+        public async Task<Product?> Handle(UpdateProductCommand command)
+        {
+            var product = await productRepository.GetByIdAsync(command.Id);
+            if (product != null)
+            {
+                product.Update(command);
+                await productRepository.UpdateAsync(product);
+                await unitOfWork.CompleteAsync();
+                return product;
+            }
+            else
+            {
+                Console.WriteLine($"Product with id {command.Id} not found");
+                throw new Exception($"Product with id {command.Id} not found");
+            }
+        }
+
+        public async Task Handle(DeleteProductCommand command)
+        {
+            var product = await productRepository.GetByIdAsync(command.Id);
+            if (product != null)
+            {
+                await productRepository.DeleteAsync(product.Id);
+                await unitOfWork.CompleteAsync();
+            }
+            else
+            {
+                Console.WriteLine($"Product with id {command.Id} not found");
+                throw new Exception($"Product with id {command.Id} not found");
+            }
+        }
+    }
---
+++ chefstock-platform/Products/Domain/Model/Commands/DeleteProductCommand.cs
@@ -0,0 +1,3 @@
+﻿namespace chefstock_platform.Products.Domain.Model.Commands;
+
+public record DeleteProductCommand(int Id);
