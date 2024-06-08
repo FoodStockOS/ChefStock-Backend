1e338e491d6f94afa7e3c67b83485ec4c90f8bdd
Joan Jefferson Talizo Balbin <joanbalbin5@gmail.com>
2024-06-08 17:51:32 -0500

    feat:  add Domain/Model/Aggregates

---
+++ chefstock-platform/Profiles/Domain/Model/Aggregates/Profile.cs
@@ -0,0 +1,33 @@
+﻿using chefstock_platform.Profiles.Domain.Model.Commands;
+using chefstock_platform.Profiles.Domain.Model.ValueObjects;
+
+namespace chefstock_platform.Profiles.Domain.Model.Aggregates;
+
+public class Profile
+{
+    public Profile()
+    {
+        Username = new Username();
+        EmailAddress = new EmailAddress();
+        Password = new Password();
+    }
+
+    public Profile(string username, string emailAddress, string password)
+    {
+        Username = new Username(username);
+        EmailAddress = new EmailAddress(emailAddress);
+        Password = new Password(password);
+    }
+
+    public Profile(CreateProfileCommand command)
+    {
+        Username = new Username(command.Username);
+        EmailAddress = new EmailAddress(command.EmailAddress);
+        Password = new Password(command.Password);
+    }
+
+    public int Id { get; }
+    public Username Username { get; private set; }
+    public EmailAddress EmailAddress { get; private set; }
+    public Password Password { get; private set; }
+}
