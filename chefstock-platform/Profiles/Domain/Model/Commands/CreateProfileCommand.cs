a14790aeca586ee3e8d320546dcdf6318c00a731
Joan Jefferson Talizo Balbin <joanbalbin5@gmail.com>
2024-06-08 17:36:55 -0500

    feat: add Model/Commands

--- chefstock-platform/Profiles/Domain/Model/Commands/CreateProfileCommand.cs
+++ chefstock-platform/Profiles/Domain/Model/Commands/CreateProfileCommand.cs
@@ -1,3 +1,3 @@
 ﻿namespace chefstock_platform.Profiles.Domain.Model.Commands;
 
-public record CreateProfileCommand(string EmailAddress, string Username, string Password);
+public record CreateProfileCommand(string EmailAddress, string Username, string Password); 
