a1e27b42b4165e50e5ab3eac174a67667cd91128
Jose Calderon <josephwantri@gmail.com>
2024-06-09 01:26:12 -0500

    feat: add RestaurantManagement bc

---
+++ chefstock-platform/RestaurantManagement/Infrastructure/Persistence/EFC/Repositories/RestaurantRepository.cs
@@ -0,0 +1,30 @@
+﻿using chefstock_platform.RestaurantManagement.Domain.Model.Aggregates;
+using chefstock_platform.RestaurantManagement.Domain.Repositories;
+using chefstock_platform.Shared.Infrastructure.Persistence.EFC.Configuration;
+using chefstock_platform.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace chefstock_platform.RestaurantManagement.Infrastructure.Persistence.EFC.Repositories;
+
+public class RestaurantRepository(AppDbContext context) : BaseRepository<Restaurant>(context), IRestaurantRepository
+{
+    public async Task<Restaurant?> GetByIdAsync(int id)
+    {
+        return await Context.Set<Restaurant>().FindAsync(id);
+    }
+
+    public async Task UpdateAsync(Restaurant restaurant)
+    {
+        Context.Set<Restaurant>().Update(restaurant);
+        await Context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var restaurant = await Context.Set<Restaurant>().FindAsync(id);
+        if (restaurant != null)
+        {
+            Context.Set<Restaurant>().Remove(restaurant);
+            await Context.SaveChangesAsync();
+        }
+    }
+}
---
+++ chefstock-platform/RestaurantManagement/Interfaces/REST/Resources/UpdateRestaurantResource.cs
@@ -0,0 +1,3 @@
+﻿namespace chefstock_platform.RestaurantManagement.Interfaces.REST.Resources;
+
+public record UpdateRestaurantResource(int Id, string Name, string Location, string Type);
